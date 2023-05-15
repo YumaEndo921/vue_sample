f5f65c0412e8e3d09e05452e1379eaf76cef84a4
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-05-15 14:04:55 +0900

    5/15_save
    保存

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 
 <script setup>
-const stock = 6;
+const clickButton = (msg) => {
+  console.log(msg);
+};
+const another = (msg) => {
+  console.log(msg);
+};
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <div v-if="stock > 5 " >まだ商品に在庫数に余裕があります</div>
-  <div v-else-if="stock === 0">申し訳ございません。現在売り切れです。</div>
-  <div v-else>在庫数が少なくなっています。お急ぎください。</div>
+  <button @click="clickButton('クリック'), another('click')">クリック</button>
 </template>
 
 <style scoped>
