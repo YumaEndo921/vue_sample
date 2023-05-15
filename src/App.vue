63e6de4c31ed72e0f274cf95eca176ba132ee2f2
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-05-15 16:53:45 +0900

    5/15_save
    学校でやった分

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 
+
 <script setup>
-const clickButton = (msg) => {
-  console.log(msg);
-};
-const another = (msg) => {
-  console.log(msg);
-};
+import { reactive, computed } from 'vue';
+const user = reactive({
+  firstName: 'John',
+  lastName: 'Doe',
+});
+const fullName = () => `${Math.random()} ${user.firstName} ${user.lastName}`;
+const cFullName = computed(
+  () => `${Math.random()} $${user.firstName} ${user.lastName}`
+);
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <button @click="clickButton('クリック'), another('click')">クリック</button>
+  <h2>fullName: {{ fullName() }}</h2>
+  <h2>fullName: {{ fullName() }}</h2>
+  <h2>fullName: {{ fullName() }}</h2>
+  <h2>cFullName: {{ cFullName }}</h2>
+  <h2>cFullName: {{ cFullName }}</h2>
+  <h2>cFullName: {{ cFullName }}</h2>
 </template>
+
+
 
 <style scoped>
 .active {
