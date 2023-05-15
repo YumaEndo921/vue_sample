6b4436fc5206a58a1536f52c51b7af0bcb551bb9
YumaEndo921 <129018764+YumaEndo921@users.noreply.github.com>
2023-05-16 00:08:25 +0900

    5/15 home

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 
 
 <script setup>
-import { reactive, computed } from 'vue';
+import { ref, reactive, computed } from 'vue';
+const count = ref(0);
+
+// const clickButton = ()=>{
+//   count ++
+// }
+
 const user = reactive({
   firstName: 'John',
   lastName: 'Doe',
 });
-const fullName = () => `${Math.random()} ${user.firstName} ${user.lastName}`;
-const cFullName = computed(
-  () => `${Math.random()} $${user.firstName} ${user.lastName}`
-);
+
+const fullName = () => {
+  console.log('Function');
+  return `${user.firstName} ${user.lastName}`;
+};
+const cFullName = computed(() => {
+  console.log('Computed Propety');
+  return `${user.firstName} ${user.lastName}`;
+});
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
   <h2>fullName: {{ fullName() }}</h2>
-  <h2>fullName: {{ fullName() }}</h2>
-  <h2>fullName: {{ fullName() }}</h2>
   <h2>cFullName: {{ cFullName }}</h2>
-  <h2>cFullName: {{ cFullName }}</h2>
-  <h2>cFullName: {{ cFullName }}</h2>
+  <!-- <input v-model="user.firstName" /> -->
+  <button @click="count++">count is : {{ count }}</button>
+
 </template>
 
 
